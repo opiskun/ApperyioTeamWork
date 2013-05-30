97b675d077909b1e6a72f5bab3b0845cd6836681
Alexandr Piskun <alexandrpiskun@gmail.com>
2013-05-29 17:12:18 -0700

    Default port is changed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT /* || 8000 */);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set('view options', { pretty: true });
